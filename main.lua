c545f5dc7927b08fff8e93ff31c322c68fe40604
chris <armoredmoose@gmail.com>
2023-02-26 17:25:56 -0600

    Added license info

--- main.lua
+++ main.lua
@@ -1,3 +1,21 @@
+--[[
+	A simple wordle clone.
+    Copyright (C) <2023>  <return5>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+--]]
+
 local ReadFile <const> = require('auxiliary.ReadFile')
 local Output <const> = require('io.Output')
 local Input <const> = require("io.Input")
